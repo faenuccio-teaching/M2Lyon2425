340900a21093f284057614b8dcc31d0e95c50e43
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-08-27 15:06:54 +0200

    first commit

---
+++ M2Lyon2425.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `M2Lyon2425` library.
+-- Import modules here that should be built as part of the library.
+import M2Lyon2425.Basic
---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "M2Lyon2425" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «M2Lyon2425» where
+  -- add any library configuration options here
