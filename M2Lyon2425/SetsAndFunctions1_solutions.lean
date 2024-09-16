ac15def94715020bef4964a3cfae0a5856a07337
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-09-16 12:45:52 +0200

    advances on part 1

--- M2Lyon2425/SetsAndFunctions1_solutions.lean
+++ M2Lyon2425/SetsAndFunctions1_solutions.lean
@@ -1,3 +1,4 @@
+import Mathlib.Algebra.Group.Nat
 import Mathlib.Data.Set.Operations
 import Mathlib.Tactic.Common
 
@@ -5,28 +6,62 @@ open Set Classical
 section Examples
 
 /- # A tautology -/
-example (α : Type) (x : α) (S : Set α) : x ∈ S ↔ S x := sorry
-
-
-
-
+example (α : Type) (x : α) (S : Set α) : x ∈ S ↔ S x := by
+  rfl
 
 
 /- # The positive integers -/
 def PositiveIntegers : Set ℤ := by
   -- intro d
-  -- -- use if 0 ≤ d then True else False --bleah!
-  -- exact (0 ≤ ·) d -- not bleah
-  -- exact @LE.le ℤ _ 0 -- uff
-  -- exact (fun d ↦ 0 ≤ d) -- mah
-  exact (0 ≤ ·) -- nicer
+  -- use if 0 < d then True else False --bleah!
+  -- exact (0 < ·) d -- not bleah
+  -- exact @LT.lt ℤ _ 0 -- uff
+  -- exact (fun d ↦ 0 < d) -- mah
+  exact (0 < ·) -- nicer
 
+example : 1 ∈ PositiveIntegers := by
+  have := Nat.zero_lt_of_ne_zero (Nat.one_ne_zero)
+  -- exact this -- why?
+  rw [← Int.ofNat_lt] at this
+  exact this --rwa
 
 /- # The even naturals -/
 def EvenNaturals : Set ℕ := by
   -- intro d
   -- exact if d % 2 = 0 then True else False
   exact (· % 2 = 0)
+
+def EvenNaturals' : Set ℕ
+  | 0 => True
+  | Nat.succ m => ¬ EvenNaturals' m
+
+
+lemma EvenEq (n : ℕ) : n ∈ EvenNaturals ↔ n ∈ EvenNaturals' := by
+  induction' n with m h_ind
+  · constructor
+    · intro _
+      trivial
+    · intro _
+      trivial
+  · constructor
+    · intro hm
+      replace hm : (m + 1) % 2 = 0 := hm --try to comment it out
+      replace hm : m % 2 = 1 :=by
+        rwa [Nat.succ_mod_two_eq_zero_iff] at hm
+      replace hm : ¬ EvenNaturals m := by
+        rw [EvenNaturals]
+        rw [hm]
+        exact Nat.one_ne_zero
+      replace h_ind := (h_ind.mpr).mt hm
+      exact h_ind
+    · intro hm
+      replace hm : ¬ EvenNaturals' m := by
+        trivial
+      replace h_ind := (h_ind.mp).mt hm
+      replace h_ind : ¬ (m % 2) = 0 := h_ind
+      rwa [Nat.mod_two_ne_zero, ← Nat.succ_mod_two_eq_zero_iff] at h_ind
+
+
 
 
 /- # An abstract set -/
@@ -44,59 +79,51 @@ example : subsub = subsub' := sorry
 
 /- # A double inclusion -/
 
-example (α : Type) (S T W : Set α) (hST : S ⊆ T) (hTW : T ⊆ W) : S ⊆ W := by sorry
+example (α : Type) (S T W : Set α) (hST : S ⊆ T) (hTW : T ⊆ W) : S ⊆ W := by
+  intro s hs
+  apply hTW
+  apply hST
+  exact hs
+  -- intro s hs
+  -- exact hTW <| hST hs -- exact hTW (hST hs)
+
+
 
 
 /- # Some exercises -/
 
-example : 1 ∉ EvenNaturals := sorry
+example : 1 ∉ EvenNaturals := by
+  intro h
+  trivial
+  -- tauto
 
-example : 1 ∈ PositiveIntegers := sorry
+example : -1 ∉ PositiveIntegers := by
+  intro h
+  trivial
 
 -- Define the set of even, positive numbers
-def EvenPositiveNaturals : Set EvenNaturals := sorry
+def EvenPositiveNaturals : Set PositiveIntegers := by
+  have P : ℤ → Prop := (· % 2 = 0)
+  rintro ⟨d, _⟩
+  exact P d
 
-example : 3 ∉ EvenPositiveNaturals := sorry --can you explain why Lean complains?
+
+example : 1 ∉ EvenPositiveNaturals := sorry --can you explain why Lean complains?
+/- Lean complains because `3` is not a term of `EvenNaturals`, so it does not make sense
+to check whether it satisifies a property defined on them. It can be made to work by writing
+`example : ⟨1, Int.zero_lt_one⟩ ∉ EvenPositiveNaturals := sorry` -/
+
+
+-- Define the set of odd numbers and prove some properties
+def OddNaturals : Set ℕ := sorry
+
+example : 3 ∈ OddNaturals := sorry
+
+example (n : ℕ) : n ∈ OddNaturals ↔ n ∉ EvenNaturals := sorry
 
 
 end Examples
 
-inductive Even : ℕ → Prop where
-  | zero    : Even 0
-  | add_two : ∀k : ℕ, Even k → Even (k + 2)
-
-inductive pp : ℕ → Type 12 where
- | z : pp 0
- | s : pp 1 → pp 2
-
-def aa : (n : ℕ) → Prop := sorry
-
-def vfd : Even := sorry
-
--- def v : aa := sorry
-
--- inductive Even' (n : ℕ) : Prop :=
---   | a : (n = 0) → Even' n
---   | b : 2 ≤ n → (Even' n-1) → Even' n
-  -- | add_two : ∀k : ℕ, Even k → Even (k + 2)
-
-def Even1 : ℕ → Prop
-  | 0 => True
-  | 1 => False
-  | n + 2 => Even n
-
-def Even2 (n : ℕ) : Prop :=
-  match n with
-  | 0 => True
-  | 1 => False
-  | n + 2 => Even n
-
-example : Even1  = Even2 := rfl
-
-
-
-
-#check (Even : Set ℕ)
 
 section Operations
 
