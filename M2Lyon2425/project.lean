11aa728803fc7e6ac3d548edf69eabb70ba0f5d4
Emilie Uthaiwat <emiliepathum@gmail.com>
2024-10-11 20:28:33 +0200

    golf + closed

--- M2Lyon2425/project.lean
+++ M2Lyon2425/project.lean
@@ -3,7 +3,6 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Topology.Defs.Basic
 import Mathlib.Topology.Constructions
 import Mathlib.Topology.Instances.EReal
-import Mathlib.Topology.Sequences
 
 /-- Goal of the project: formalize the counterexamples of the chapter 10
 of the book "Counterexamples in Analysis" by Bernard R. Gelbaum and
@@ -12,28 +11,27 @@ John M. H. Olmsted -/
 -- Counterexample 1: Two disjoint closed sets that are at
 -- a zero distance
 
-def F1 : Set (ℝ × ℝ) := setOf (fun (x,y) ↦ x*y = 1)
+def F1 : Set (ℝ × ℝ) := setOf fun (x,y) ↦ x*y = 1
 
-def F2 : Set (ℝ × ℝ) := setOf (fun (x,y) ↦ y=0)
+def F2 : Set (ℝ × ℝ) := setOf fun (_,y) ↦ y=0
 
 lemma F1_disjoint_F2 : F1 ∩ F2 = ∅ := by
   by_contra H
   rw [Set.ext_iff] at H
   push_neg at H
-  cases H with
-  | intro w h =>
-      cases h with
-      | inl h =>
-          cases' h with h₁ h₂
-          cases' h₁ with h h'
-          change w.2 = 0 at h'
-          change w.1 * w.2 = 1 at h
-          rw [h', mul_zero] at h
-          exact zero_ne_one h
-      | inr h => exact h.2
+  cases' H with w h
+  cases h with
+  | inl h =>
+      cases' h with h₁ _
+      cases' h₁ with h h'
+      change w.2 = 0 at h'
+      change w.1 * w.2 = 1 at h
+      rw [h', mul_zero] at h
+      exact zero_ne_one h
+  | inr h => exact h.2
 
-instance : TopologicalSpace (ℝ × ℝ) := by
-  have : TopologicalSpace ℝ := by
-    have := EReal.instTopologicalSpace
-    exact TopologicalSpace.induced Real.toEReal this
-  exact @instTopologicalSpaceProd ℝ ℝ this this
+lemma F1_closed : IsClosed F1 :=
+  isClosed_eq (Continuous.mul continuous_fst continuous_snd) continuous_one
+
+lemma F2_closed : IsClosed F2 :=
+  isClosed_eq continuous_snd continuous_zero
