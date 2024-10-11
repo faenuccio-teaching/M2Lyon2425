59de3384a81c91d4cdf549e954ca6647e1b8d65e
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-10-11 15:43:36 +0200

    updated lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package "M2Lyon2425" where
   ]
   -- add any additional package configuration options here
 
-require "leanprover-community" / "mathlib"
+require "leanprover-community" / "mathlib" @ "bf1047068974f5ebc957a30d55f6ff5d3989f0d3"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git
