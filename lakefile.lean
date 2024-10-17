46e1531ac7591773366afab39fa7556967ea663d
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-10-17 19:28:21 +0200

    antoher trial

--- lakefile.lean
+++ lakefile.lean
@@ -13,7 +13,7 @@ require "leanprover-community" / "mathlib" @ git "bf1047068974f5ebc957a30d55f6ff
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
+  "https://github.com/leanprover/doc-gen4" @ "d684d596bcf8fbb114320776b80a1dbfce0a0786"
 
 @[default_target]
 lean_lib «M2Lyon2425» where
