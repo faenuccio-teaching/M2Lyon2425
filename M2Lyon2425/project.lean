8146d0915970ca6bdaf1f84834ee0037ab2fbbec
Emilie Uthaiwat <emiliepathum@gmail.com>
2024-10-21 11:10:04 +0200

    progress

--- M2Lyon2425/project.lean
+++ M2Lyon2425/project.lean
@@ -3,6 +3,7 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Topology.Defs.Basic
 import Mathlib.Topology.Constructions
 import Mathlib.Topology.Instances.EReal
+import Mathlib.Analysis.SpecialFunctions.Pow.Real
 
 /-- Goal of the project: formalize the counterexamples of the chapter 10
 of the book "Counterexamples in Analysis" by Bernard R. Gelbaum and
@@ -35,3 +36,29 @@ lemma F1_closed : IsClosed F1 :=
 
 lemma F2_closed : IsClosed F2 :=
   isClosed_eq continuous_snd continuous_zero
+
+noncomputable instance : Dist (ℝ × ℝ) where
+  dist := fun p q ↦ ((q.1 - p.1)^(2 : ℝ) + (q.2 - p.2)^(2 : ℝ))^(1/(2 : ℝ))
+
+def set_dist (A B : Set (ℝ × ℝ)) : Set ℝ := setOf (∃ p ∈ A, ∃ q ∈ B, Dist.dist p q = ·)
+
+noncomputable def distance (A B : Set (ℝ × ℝ)):= InfSet.sInf (set_dist A B)
+
+lemma dist_F1_F2_eq_zero : distance F1 F2 = 0 := by
+  have h : ∀ x ∈ set_dist F1 F2, 0 ≤ x := sorry
+  have h₂ := Real.sInf_nonneg _ h
+  have h₃ : ¬0 < sInf (set_dist F1 F2) := by
+    intro h₃
+    have h₄ : sInf (set_dist F1 F2) ∈ lowerBounds (set_dist F1 F2) := by
+      change ∀ a, a ∈ (set_dist F1 F2) → sInf (set_dist F1 F2) ≤ a
+      intro a ha
+      sorry
+    change ∀ a, a ∈ (set_dist F1 F2) → sInf (set_dist F1 F2) ≤ a at h₄
+    let ε := sInf (set_dist F1 F2)
+    specialize h₄ (Dist.dist (2/ε, ε/2) (2/ε, 0))
+    have h₅ : dist (2 / ε, ε / 2) (2 / ε, 0) ∈ set_dist F1 F2 := sorry
+    specialize h₄ h₅
+    change ε ≤ ((2/ε - 2/ε)^2 + (0 - ε/2)^2)^(1/2) at h₄
+    sorry
+  have h₄ := eq_of_le_of_not_lt h₂ h₃
+  exact h₄.symm
