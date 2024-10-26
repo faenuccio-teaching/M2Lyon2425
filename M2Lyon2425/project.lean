b6ae0358d88e2894572c98dd1b3f1a58197d7631
Emilie Uthaiwat <emiliepathum@gmail.com>
2024-10-26 13:15:25 +0200

    golf

--- M2Lyon2425/project.lean
+++ M2Lyon2425/project.lean
@@ -20,12 +20,10 @@ lemma F1_disjoint_F2 : F1 ∩ F2 = ∅ := by
   by_contra H
   rw [Set.ext_iff] at H
   push_neg at H
-  cases' H with w h
-  cases h with
+  cases' H with w H
+  cases H with
   | inl h =>
-      cases' h with h₁ _
-      cases' h₁ with h h'
-      change w.2 = 0 at h'
+      obtain ⟨⟨h, h'⟩, _⟩ := h
       change w.1 * w.2 = 1 at h
       rw [h', mul_zero] at h
       exact zero_ne_one h
@@ -40,72 +38,38 @@ lemma F2_closed : IsClosed F2 :=
 noncomputable instance : Dist (ℝ × ℝ) where
   dist := fun p q ↦ ((q.1 - p.1)^(2 : ℝ) + (q.2 - p.2)^(2 : ℝ))^(1/(2 : ℝ))
 
-def set_dist (A B : Set (ℝ × ℝ)) : Set ℝ := setOf (∃ p ∈ A, ∃ q ∈ B, Dist.dist p q = ·)
+def set_dist (A B : Set (ℝ × ℝ)) : Set ℝ := setOf (∃ p ∈ A, ∃ q ∈ B, dist p q = ·)
 
-noncomputable def distance (A B : Set (ℝ × ℝ)):= InfSet.sInf (set_dist A B)
+noncomputable def distance (A B : Set (ℝ × ℝ)) := sInf (set_dist A B)
 
-lemma dist_F1_F2_less_zero : distance F1 F2 ≤ 0 := by
-  rw [distance, Real.sInf_le_iff]
-  · intros ε hε
-    use dist (2 / ε, ε / 2) (2 / ε, 0)
-    refine ⟨?_, ?_⟩
-    · use (2 / ε, ε / 2)
-      refine ⟨?_, ?_⟩
-      change (2/ε) * (ε/2) = 1
-      norm_num
-      exact div_self (ne_of_gt hε)
-      use (2 / ε, 0)
-      refine ⟨?_, rfl⟩
-      change 0 = 0
-      rfl
-    · rw [zero_add]
-      change ((2/ε - 2/ε)^2 + (0 - ε/2)^2)^(1/2) < ε
-      rw [sub_self, zero_sub, Real.zero_rpow two_ne_zero, zero_add]
-      simp only [Real.rpow_two, even_two, Even.neg_pow]
-      rw [← Real.sqrt_eq_rpow, Real.sqrt_sq]
-      simp only [half_lt_self_iff]
-      exact hε
-      linarith
-  · rw [BddBelow, lowerBounds, Set.Nonempty]
-    use 0
-    dsimp
-    intros a ha
-    obtain ⟨p, ⟨_, ⟨q, ⟨_, hdist⟩⟩⟩⟩ := ha
-    rw [← hdist]
-    change 0 ≤ ((q.1 - p.1)^(2 : ℝ) + (q.2 - p.2)^(2 : ℝ))^(1/(2 : ℝ))
-    rw [← Real.sqrt_eq_rpow]
-    exact Real.sqrt_nonneg _
-  · rw [Set.Nonempty]
-    use dist ((2 : ℝ), (1 : ℝ) / (2 : ℝ)) ((2 : ℝ), (0 : ℝ))
-    use (2, 1 / 2)
-    refine ⟨?_, ?_⟩
-    change 2 * (1/2) = 1
+lemma set_dist_nonempty : (set_dist F1 F2).Nonempty := by
+  refine ⟨dist (2, 1/2) ((2 : ℝ), (0 : ℝ)), ⟨(2, 1/2), ⟨?_, ⟨(2,0),
+    ⟨(by rw [Set.mem_def]; rfl), rfl⟩⟩⟩⟩⟩
+  change 2 * (1/2) = 1
+  norm_num
+
+lemma dist_on_prod_pos : ∀ d ∈ set_dist F1 F2, 0 ≤ d := by
+  rintro a ⟨p, ⟨_, ⟨q, ⟨_, hdist⟩⟩⟩⟩
+  rw [← hdist]
+  change 0 ≤ ((q.1 - p.1)^2 + (q.2 - p.2)^2)^(1/2)
+  rw [← Real.sqrt_eq_rpow]
+  exact Real.sqrt_nonneg _
+
+lemma distance_F1_F2_neg : distance F1 F2 ≤ 0 := by
+  rw [distance, Real.sInf_le_iff ⟨0, dist_on_prod_pos⟩ set_dist_nonempty]
+  refine fun ε hε ↦ ⟨dist (2 / ε, ε / 2) (2 / ε, 0), ⟨?_, ?_⟩⟩
+  · refine ⟨(2 / ε, ε / 2), ⟨?_, ⟨(2 / ε, 0), ⟨(by rw [Set.mem_def]; rfl), rfl⟩⟩⟩⟩
+    change (2/ε) * (ε/2) = 1
     norm_num
-    use (2,0)
-    refine ⟨?_, ?_⟩
-    change 0 = 0
-    rfl
-    rfl
+    exact div_self (ne_of_gt hε)
+  · change ((2/ε - 2/ε)^2 + (0 - ε/2)^2)^(1/2) < 0 + ε
+    rw [zero_add, sub_self, zero_sub, Real.zero_rpow two_ne_zero, zero_add, Real.rpow_two,
+    Even.neg_pow even_two, ← Real.sqrt_eq_rpow, Real.sqrt_sq, half_lt_self_iff]
+    exact hε
+    linarith
 
-lemma zero_less_dist_F1_F2 : 0 ≤ distance F1 F2 := by
-  apply le_csInf
-  · rw [Set.Nonempty]
-    use dist ((2 : ℝ), (1 : ℝ) / (2 : ℝ)) ((2 : ℝ), (0 : ℝ))
-    use (2, 1 / 2)
-    refine ⟨?_, ?_⟩
-    change 2 * (1/2) = 1
-    norm_num
-    use (2,0)
-    refine ⟨?_, ?_⟩
-    change 0 = 0
-    rfl
-    rfl
-  · intros a ha
-    obtain ⟨p, ⟨_, ⟨q, ⟨_, hdist⟩⟩⟩⟩ := ha
-    rw [← hdist]
-    change 0 ≤ ((q.1 - p.1)^(2 : ℝ) + (q.2 - p.2)^(2 : ℝ))^(1/(2 : ℝ))
-    rw [← Real.sqrt_eq_rpow]
-    exact Real.sqrt_nonneg _
+lemma distance_F1_F2_pos : 0 ≤ distance F1 F2 :=
+  le_csInf set_dist_nonempty dist_on_prod_pos
 
-lemma dist_F1_F2_eq_zero : distance F1 F2 = 0 :=
-  ((LE.le.ge_iff_eq zero_less_dist_F1_F2).1 dist_F1_F2_less_zero).symm
+lemma distance_F1_F2_eq_zero : distance F1 F2 = 0 :=
+  (LE.le.ge_iff_eq distance_F1_F2_neg).1 distance_F1_F2_pos
