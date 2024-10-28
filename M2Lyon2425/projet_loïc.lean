88d164e8b9c647749aa636936c6c416a55df15c2
LMosticone <lo.mosticone@gmail.com>
2024-10-28 09:50:07 +0100

    projet

---
+++ M2Lyon2425/projet_loïc.lean
@@ -0,0 +1,37 @@
+/-
+Bonjour
+Mon idée de projet se porte sur le théorème de Cauchy sur les groupe (pour tout diviseur premier du cardinal d'un
+goupe fini , il existe un élément de cet ordre)
+
+La preuve que je voudrais formaliser est celle avec les actions de groupe :
+on fait agir le groupe le groupe cyclique sur un certain ensemble et on peux en déduire par
+l'équation aux classes l'existence d'un tel élément
+
+Je n'ai pas encore trouvé de livres reprenant éxactement la preuve que j'avais en tête
+mais l'idée de la preuve se trouve sur wikipédia .
+
+Le théorème se trouve déjà sur mathlib sous le nom " _root_.exists_prime_orderOf_dvd_card" avec une preuve
+qui est je pense celle que je veux formaliser mais j'avoue de pas vraiment tout comprendre de ce qui est écrit
+
+Je pense qu'une des diffucultés de la preuve est le fait que certains argument (de dénombrement notamment)
+sont rarement très rigoureux dans cette preuve et les formaliser pourrait être difficile.
+
+
+Si jamais tout se passerait mieux que prévu et que la preuve se révélerait "trop facile" , je pourrais aussi
+formaliser ne autre preuve de se résultat par récurrence sur le cardinal du groupe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-/
