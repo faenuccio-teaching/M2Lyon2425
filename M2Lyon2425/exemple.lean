c0dccae0cb5c5ffdfbe0a4f85118aea7e1b259c3
Timothée Huneau <timothee.huneau@etu.univ-lyon1.fr>
2024-11-06 15:55:30 +0100

    Ajout exemple union de fonctions

--- M2Lyon2425/exemple.lean
+++ M2Lyon2425/exemple.lean
@@ -3,31 +3,43 @@ import Mathlib
 variable {α : Type*} (f : α → α → Prop)
 
 @[simp]
-def iteration (f : α → α → Prop) (n : ℕ) : α → α → Prop :=
+def iteration (n : ℕ) : α → α → Prop :=
   match n with
   | 0 => (· = ·)
-  | m+1 => fun x y ↦ (∃ w, (iteration f m) x w ∧ f w y)
+  | m+1 => fun x y ↦ (∃ w, (iteration m) x w ∧ f w y)
 
 
 lemma equiv_defs :
   ∀ n, ∀ x y,
   iteration f n x y →
-  ∃ (w : ℕ → α), (w 0 = x ∧ w n = y) ∧ (∀ i, n < i+1 ∨ f (w i) (w (i+1))) := by
+  ∃ (w : ℕ → α), -- `w` est définie sur ℕ, mais on ne s'intéresse qu'à ses `n` premières valeurs
+    (w 0 = x ∧ w n = y) ∧ -- on veut forcer des conditions sur les valeurs en 0 et en `n`
+    (∀ i,
+      n < i+1 ∨ -- et la condition ne s'applique que si `i < n`, donc pas si `n < i+1`
+      f (w i) (w (i+1))) := by
+
     intro n
     induction n with
-    | zero => sorry -- le problème n'est pas ici, cette partie est "facile"
+    | zero => sorry -- le problème n'est pas ici, cette partie est "facile", d'où le `sorry`
     | succ m hm =>
-      intro x y hiter
-      simp at hiter
-      let w_der := hiter.choose
-      have ⟨hw_der_prev, hw_der_suiv⟩ := hiter.choose_spec
+      intro x y hite
+      simp at hite
+      let w_der := hite.choose
+      have ⟨hw_der_prev, hw_der_suiv⟩ := hite.choose_spec
       have := hm x w_der hw_der_prev
       let w := this.choose
       have hw := this.choose_spec
+      -- on construit ici une fonction `w'` qui étend `w`
       let w' := fun i ↦ (match i with
         | m+1 => y
         | _ => w i
         )
       use w'
-
-      sorry
+      simp
+      constructor
+      · -- on sait que `w' 0 = w 0` car 0 n'est pas un successeur (cf def. de `w'`)
+        -- mais comment le montrer ?
+        calc
+          w' 0 = w 0 := sorry
+          _ = x := hw.left.left
+      · sorry -- le même problème apparait, à résoudre similairement
