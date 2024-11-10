8fc88c72622288ca71d5fc4978ef45dbdab25d82
LMosticone <lo.mosticone@gmail.com>
2024-11-10 15:55:48 +0100

    ajout_squelette

--- M2Lyon2425/projet_loïc.lean
+++ M2Lyon2425/projet_loïc.lean
@@ -18,8 +18,23 @@ sont rarement très rigoureux dans cette preuve et les formaliser pourrait être
 
 
 Si jamais tout se passerait mieux que prévu et que la preuve se révélerait "trop facile" , je pourrais aussi
-formaliser ne autre preuve de se résultat par récurrence sur le cardinal du groupe
+formaliser une autre preuve de se résultat par récurrence sur le cardinal du groupe
 
+
+Voici le squelette de la preuve que je proposer de formaliser , la preuve se trouve dans le livre "Théorie des groupes"
+de Félix Ulmer.
+
+
+Soit G un groupe fini et p un nombre premier divisant le cardinal de G
+On considère H le sous groupe engendré par le p-cycle (1,2,3,...,p) dans Sₚ .
+On considère X le sous ensemble des p-uplet (g₁,g₂,...,gₚ) de G tel que g₁g₂...gₚ = e
+On a alors une action de H sur X par permutations cyclique des composantes : (1,2,3,...,p) ⬝ (g₁,g₂,...,gₚ) = (gₚ,g₁,g₂,...,g_p-1)
+L'élément gₚ étant déterminé par les p-1 autres éléments , on a que X est de cardinal |G|^(p-1)
+On utilise ici un resultat général sur les p-groupe : |Xᴴ| ≅ |X| [mod p] où Xᴴ désigne les point fixes de X sous l'action de H
+Ce résultat se démontre grâce à l'équation aux classes.
+On a alors p ∣  |Xᴴ| car p ∣ |X|
+Or , (e,e,...,e) ∈ Xᴴ donc cet ensemble est non vide et donc de cardinal au moins p .
+Les éléments de Xᴴ étant de la forme (g,g,...,g), on a bien l'éxistence d'un élément d'ordre p dans G
 
 
 
