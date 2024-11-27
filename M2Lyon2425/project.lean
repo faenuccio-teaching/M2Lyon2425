5df20ba3a5dd8a0f14389613a18061cbbf705c18
Shraze97 <suryanshs@iisc.ac.in>
2024-11-27 08:02:03 +0100

    some update

--- M2Lyon2425/project.lean
+++ M2Lyon2425/project.lean
@@ -34,16 +34,61 @@ instance LinCombEquiv : Equivalence (isLinearCombination h) where
   symm := by
     intros x y hxy
     rw[isLinearCombination] at *
-    let l := hxy.choose
-    let l' : ℕ →₀ ℝ := {
+    set l := hxy.choose with hl
+    set l' : ℕ →₀ ℝ := {
       support := l.support,
-      toFun := λ i => - (l i),
+      toFun := λ i => - (l.toFun i),
       mem_support_toFun := by
         intro i
-        sorry
-    }
+        rw[l.mem_support_toFun]
+        simp only [ne_eq, neg_eq_zero]
+    } with hl'
+    use l'
+    rw[hl']
+    simp only [Finsupp.coe_mk, smul_eq_mul, neg_mul, Finset.sum_neg_distrib]
+    have hll := hxy.choose_spec
+    rw[←hl] at hll
+    simp only [Finsupp.coe_mk,smul_eq_mul] at hll
+    rw[Finsupp.coe_mk] at hll
+    have hl': l = Classical.choose hxy := by rfl
+    rw[← hl'] at hll
+    rw[← hll]
+    simp only [neg_sub]
+  trans := by
+    intros x y z hxy hyz
+    rw[isLinearCombination] at *
 
     sorry
-  trans := sorry
+
+/-Now having made these equivalences, we use Setoid.Classes to make classes on these elements, then creating an
+Setoid.IndexedPartition  on ℝ  -/
+
+/-Here, ℝ = ⋃ Eᵅ  where α is chosen from a particular Eᵃ , which becomes our indexing set-/
+/-Prove that Eᵅ is countable for each α   -/
+/- Define a ennumeration {xᵢᵅ} for each Eᵅ. with α = x₁ᵅ -/
+/-Let Eᵅᵢ = {xᵢᵅ} ∪  ⋃ⱼ {xᵢᵅ + hⱼ} , where j ∈ ℕ   -/
+
+/-Prove that Eᵅ = ⋃ᵢ Eᵅᵢ , where i ∈ ℕ -/
+/-Let Rᵅₘ = Eᵅₘ \ ⋃ⱼ Eᵅⱼ where j ∈ {1,2..m-1} if m ≥ 2  and Rᵅ₁ = Eᵅ₁.-/
+
+/- Prove that Eᵅ = ⋃ₘ Rᵅₘ. make this an indexed partition -/
+
+/- # Defining F on Eᵅ-/
+
+/-For each  x ∈  Rᵅₘ, if x ≠ xᵅₘ , then F(x) = F(xₘᵅ) + (x - xₘᵅ)f(xₘᵅ)  otherwise  F(x) = 0 where x = xₘᵅ-/
+/-lift this F to Eᵅ by using Indexed.PartitionPiecewise-/
+
+/-# Defining F on ℝ -/
+/-Lift this F to ℝ by  using Indexed.PartitionPiecewise.-/
+
+/-If x ∈ ℝ → ∃ n,α st. x = xᵅₘ -/
+
+/-Prove that there exists some N₀ st. xᵅₘ  + hₙ ∈ Rᵅₘ ∀ n ≥ N₀ -/
+/-Proof Sketch :- consider an open interval I st. xᵅⱼ ∉ I for j ≤ m-1 (If m = 1, our case is already satisfied). -/
+/- Then choose N₁ st.  Aⱼ = {xᵅⱼ + hₙ, n ≥ N₁} where j≤m and Aⱼ∩ Aₘ ∀ j. Then remove all  the finite points from
+  Aₘ that come from Bⱼ = {xᵅⱼ + hₙ, n ≤  N₁} where j≤m, call it A-/
+/-A contains a set Cₘ st. ∃ ℕ₂ C = {xᵅⱼ + hₙ, n ≥ ℕ₂}. Then C ⊆ Rᵅₘ  -/
+/-Thus ∀ n ≥ N₂
+F(xₘᵅ +hₙ) - F(xₘᵅ) / hₙ  = (F(xₘᵅ) + (xᵅₘ + hₙ - xₘᵅ)f(xₘᵅ) - F(xₘᵅ))/hₙ  = f(xᵅₘ)ₙ-/
 
 -- end CounterExample1
