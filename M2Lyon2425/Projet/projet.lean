2b4f287c13862e1cbe2cd2ba4b3c1592e564cd77
LMosticone <lo.mosticone@gmail.com>
2024-12-01 14:20:38 +0100

    aa

--- M2Lyon2425/Projet/projet.lean
+++ M2Lyon2425/Projet/projet.lean
@@ -6,11 +6,45 @@ import Mathlib.Tactic
 import Mathlib.Data.Real.Basic
 import Mathlib.GroupTheory.Perm.Basic
 import Mathlib.GroupTheory.Sylow
-import Mathlib.LinearAlgebra.Matrix
+--import Mathlib.LinearAlgebra.Matrix
 variable {p : ℕ }
-variable (G : Type*) [Group G]
+variable (G : Type*)  [Group G]
 
 #print Sylow
-example : GeneralLinearGroup
+#check MulAction.QuotientAction
+def conjugate  (x : G) (H : Subgroup G) : Subgroup G where
+  carrier := {a : G | ∃ h, h ∈ H ∧ a = x * h * x⁻¹}
+  one_mem' := by
+    dsimp
+    use 1, H.one_mem
+    group
+  inv_mem' := by
+    dsimp
+    intro x hx
+    obtain ⟨h, mem, cond⟩ := hx
+    use h⁻¹, H.inv_mem mem
+    rw [cond]; group
+  mul_mem' := by
+    dsimp
+    intro x y hx hy
+    obtain ⟨h₁, mem₁, cond₁⟩ := hx
+    obtain ⟨h₂, mem₂, cond₂⟩ := hy
+    use h₁ * h₂, H.mul_mem mem₁ mem₂
+    rw [cond₁, cond₂]; group
+
+instance (H : Subgroup G) (S : Sylow  p G) : Sylow p H where
+  carrier := by sorry
+  mul_mem' := by sorry
+  inv_mem' := by sorry
+  one_mem' := by sorry
+  isPGroup' := by sorry
+  is_maximal' := by sorry
+
+
 theorem sylow_of_subgroup (H : Subgroup G) (S : Sylow  p G) :
-    ∃ g , (H  ∩ ) ∈  Sylow p H := by sorry
+    H ∈   (Sylow p G).toSubgroup := by sorry
+
+
+
+theorem sylow_of_subgroup (H : Subgroup G) (S : Sylow  p G) :
+    ∃ g ∈ G , H ⊓ (conjugate g (Sylow.toSubgroup S)) ∈  (Sylow p H.toGroup) := by
