97143dd26f4a7fb947087dfc2f4a3a81872f0311
LMosticone <lo.mosticone@gmail.com>
2025-01-05 15:36:39 +0100

    a

--- M2Lyon2425/Projet/projet.lean
+++ M2Lyon2425/Projet/projet.lean
@@ -37,19 +37,49 @@ def conjugate {G : Type*} [Group G] (x : G) (H : Subgroup G) : Subgroup G where
 def Subgroup.Setoid {G : Type*} [Group G](B : Subgroup G) : Setoid G  where
   r := fun x y ↦ x*y⁻¹ ∈ B
   iseqv := {
-    refl := by sorry
-    symm := by sorry
-    trans := by sorry
+    refl := by
+      intro x
+      rw[mul_inv_cancel]
+      exact B.one_mem
+    symm := by
+      intro x y h1
+      apply B.inv_mem at h1
+      rw[mul_inv_rev x y⁻¹,inv_inv] at h1
+      exact h1
+    trans := by
+      intro x y z h1 h2
+      have h3 := B.mul_mem h1 h2
+      rw[mul_assoc,← mul_assoc y⁻¹ y z⁻¹,inv_mul_cancel,one_mul] at h3
+      exact h3
+
+
   }
 /--une définition des sylow sous forme d'une prop-/
 structure IsSylow  (G : Type*)  [Group G] (H : Subgroup G): Prop where
   isPgroup : ∃ k : ℕ , Nat.card H = p^k
   isMaximal : ¬ (p ∣  H.index )
+noncomputable
+instance {G : Type*} [Group G] (H : Subgroup G)(S : Sylow  p G) : MulAction H (Quotient (Sylow.toSubgroup S).Setoid ) where
+  smul := fun x y => Quotient.mk (Sylow.toSubgroup S).Setoid (x * (Quot.out y))
+  one_smul := by
+    intro b
+    rw[smul]
+
+
+  mul_smul := by sorry
 
 /-- dans la prop 2.2 , le fait que le stabilisateurs des point de X sous l'action de H sont de la forme H ∩ gSg⁻¹-/
 theorem stab {G : Type*} [Group G] (H : Subgroup G) (S : Sylow  p G)  (h : H) :
-    MulAction.stabilizer H  (Quotient.mk   (Subgroup.Setoid (Sylow.toSubgroup S)) h)   =  Subgroup.subgroupOf (conjugate  (h : G) (Sylow.toSubgroup S)) H := by sorry
-
+    MulAction.stabilizer H  (Quotient.mk   (Subgroup.Setoid (Sylow.toSubgroup S)) h)   =  Subgroup.subgroupOf (conjugate  (h : G) (Sylow.toSubgroup S)) H := by
+    ext x
+    simp
+    constructor
+    · intro h1
+      sorry
+    · intro h2
+      sorry
+lemma orbite {G : Type*} [Group G] (H : Subgroup G) (S : Sylow  p G) : ∃ h : H , ¬ (p ∣ Nat.card (Quotient.mk   (Subgroup.Setoid (Sylow.toSubgroup S)) h)) :=
+  by sorry
 
 /-- prop 2.2-/
 theorem sylow_of_subgroup {G : Type*} [Group G]  (H : Subgroup G) (S : Sylow  p G) :
@@ -75,5 +105,5 @@ theorem exist_sylow_of_subgroup {G : Type*} [Group G] (H : Subgroup G) ( S : Syl
      -- have :∃ n, Subgroup.index H = p ^ n :=  IsPGroup.index hQ ((conjugate w ↑S).subgroupOf H : Subgroup Q)
 
 #check Equiv.Perm.subgroupOfMulAction
-#check Nat.Prime
+
  --theorem imbedd_to_gln {G : Type*} [Group G]
