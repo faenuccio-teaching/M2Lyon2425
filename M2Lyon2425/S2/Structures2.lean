902080cafe20f5c8891085525ca5429f7a4b11c8
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-01-10 16:23:57 +0100

    wip

--- M2Lyon2425/S2/Structures2.lean
+++ M2Lyon2425/S2/Structures2.lean
@@ -31,7 +31,7 @@ instance (M N : Type*) [AddCommGroup M] [AddCommGroup N] [NormedModule_bad M] [N
   norm_b := fun ⟨m, n⟩ ↦ max ‖m‖₁ ‖n‖₁
 
 instance (M N : Type*) [ModuleWithRel M] [ModuleWithRel N] : ModuleWithRel (M × N) where
-      rel := fun ⟨m1, n1⟩ ⟨m2, n2⟩ ↦ (rel m1 m2) ∧ (rel n1 n2)
+  rel := fun ⟨m1, n1⟩ ⟨m2, n2⟩ ↦ (rel m1 m2) ∧ (rel n1 n2)
 
 instance (M : Type*) [AddCommGroup M] [NormedModule_bad M] : ModuleWithRel M where
   rel := fun m n ↦ ‖m - n‖₁ ≤ 1
@@ -42,20 +42,24 @@ example (hp : ∀ M : Type, [AddCommGroup M] → [NormedModule_bad M] → ∀ m 
     (M : Type) [AddCommGroup M] [NormedModule_bad M] (v : M × M) : p (rel v) := by
   specialize hp (M × M) v
   -- exact hp
+/-This is not working because the `rel` in the goal comes from the `ModuleWithRel` instance on a
+product, whereas the `rel` in `hp` comes from the `Rel` instance *deduced* from the
+`NormedModule_bad` instance on the product (it suffices to hover on the terms to see this).
+-/
   sorry
 
-/- Let's try again! The problem is that passing from `NormedModule₁` to `ModuleWithRel`
+/- Hot to solve this? The problem is that passing from `NormedModule₁` to `ModuleWithRel`
 (i.e. declaring a `ModuleWithRel` instance on every `NormedModule₁`)
 is not a pure "erasure", because we are not simply throwing away a field, but using some
 field in the first (namely `‖ ⬝ ‖₁`) to construct the term `rel` of the second: this yields to the
 problem we have just witnessed, and the slogan is that only "forgetful inheritance" is allowed: this
 is an idea initially proposed by Affeldt, Cohen, Kerjean, Mahboubi, Rouhling and Sakaguchi in
-https://inria.hal.science/hal-02463336v2 , from which this example is extracted. In their words, one
+https://inria.hal.science/hal-02463336v2, from which this example is extracted. In their words, one
 
-needs to ensure definitional equality by including poorer structures into richer ones; this way,
-"deducing" one structure from the other always amounts to erasure of data, and this guarantees
+" needs to ensure definitional equality by including poorer structures into richer ones; this way,
+deducing one structure from the other always amounts to erasure of data, and this guarantees
 there is a unique and canonical way of getting it. We call this technique *forgetful inheritance*,
-as it is reminiscent of forgetful functors in category theory.
+as it is reminiscent of forgetful functors in category theory."
 -/
 
 class NormedModule₁ (M : Type*) [AddCommGroup M] where
@@ -63,7 +67,10 @@ class NormedModule₁ (M : Type*) [AddCommGroup M] where
   norm₁ : M → ℝ
 
 instance (M : Type*) [AddCommGroup M] [NormedModule₁ M] : ModuleWithRel M := ⟨NormedModule₁.rel⟩
-
+/- The huge difference with what happened for `NormedModule_bad` is that there the instance
+contained some **new** data (the definition of `rel`), whereas here it is simply a projection, or the
+forgetfullness of `norm₁`. No *creation*!
+-/
 
 -- Let's inspect the difference: in one case we need a `Rel`, in the other we need an `AddCommGroup`
 #print NormedModule_bad
@@ -83,8 +90,16 @@ variable (p : ∀ {T : Type}, (T → Prop) → Prop)
 example (hp : ∀ M : Type, [AddCommGroup M] → [NormedModule₁ M] → ∀ m : M, p (rel m))
     (M : Type) [AddCommGroup M] [NormedModule₁ M] (v : M × M) : p (rel v) := by
   specialize hp (M × M) v
-  -- the `rel v` in the goal is the `rel` field of the `NormedModule₁ `M × M`; and the one in `hp`
-  -- is
+  /-the `rel v` in the goal is still the `rel` coming from the `ModuleWithRel` instance on a
+product, and the `rel` in `hp` still comes from the `ModuleWithRel` instance deduced from the
+`NormedModule₁ structure on `M × M`; but now this second instance is simply obtained from the first
+by forgetting a field, so in particular it *coincides definitionally* with the previous one.
+
+Indeed, observe that in the first case we were forced to manually define a `ModuleWithRel` instance
+on the product, whereas now the path has been
+`?m : ModuleWithRel (M × M)` ← `?m : NormedModule₁ (M × M)` ← *the instance above*
+-->**NO questo è falso, ma probabilmente non troppo: il modo in cui si crea la instance di `ModuleWithRel` cambia nei due casi**
+  -/
   exact hp
 
 /- The **same example** can be constructed using the `extend` construction:-/
