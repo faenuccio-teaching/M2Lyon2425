19962ad14b49627f53d965fe707b4a370f1eb06f
morel <smorel@math.princeton.edu>
2025-02-05 19:45:19 +0100

    revert

--- M2Lyon2425/S2/Categories1.lean
+++ M2Lyon2425/S2/Categories1.lean
@@ -1,4 +1,4 @@
-import Mathlib.CategoryTheory.Comma.StructuredArrow.Basic
+import Mathlib.CategoryTheory.Comma.StructuredArrow
 import Mathlib.CategoryTheory.Limits.Shapes.Terminal
 import Mathlib.GroupTheory.Abelianization
 import Mathlib.Topology.Category.TopCat.Basic
