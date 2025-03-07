a416e21b9127de9c9783406a7a594db873b76c25
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-03-07 16:10:25 +0100

    modified State

--- M2Lyon2425/S2/Fibonacci_Siddhartha.lean
+++ M2Lyon2425/S2/Fibonacci_Siddhartha.lean
@@ -1,46 +1,45 @@
 import Mathlib.Tactic
 
 
-structure faeState (σ α : Type*) where
-  run : σ → α × σ
 
-instance (σ : Type*) : Monad (faeState σ) where
-  pure x := ⟨fun s => (x, s)⟩
-  bind x f := ⟨fun s =>
-    let (a, s') := x.run s
-    (f a).run s'⟩
+abbrev State' (σ α : Type*) := σ → σ × α
 
-def faeFibM_old (n : ℕ) : faeState (List ℕ) ℕ := do
-  ⟨fun L ↦ match L.get? n with
-    | some y => ⟨y, L⟩ -- case when `n < L.length`
+instance (σ : Type*) : Monad (State' σ) where
+  pure a := fun s => (s, a)
+  bind x f := fun s ↦
+    let (s', a) := x s
+    f a s'
+
+
+def faeFibM_old' (n : ℕ) : State' (List ℕ) ℕ := do
+  fun L ↦ match L.get? n with
+    | some y => ⟨L, y⟩ -- case when `n < L.length`
     | none => match n with -- case when `L.length ≤ n`
-      | 0 => ⟨1, [1]⟩ -- subcase when `L` is empty
-      | 1 => ⟨1, [1, 1]⟩ -- subcase when `L` is a singleton
+      | 0 => ⟨[1], 1⟩ -- subcase when `L` is empty
+      | 1 => ⟨[1, 1], 1⟩ -- subcase when `L` is a singleton
       | k + 2 =>
-        let sum : ℕ := ((faeFibM_old k).1 L).1 + ((faeFibM_old (k+1)).1 L).1
-        ⟨sum, L ++ [sum]⟩ -- subcase when `L` has at least two elements
-    ⟩
-
-instance : Repr (faeState (List ℕ) ℕ) :=  ⟨fun L n ↦ instReprNat.reprPrec (L.1 []).1 n⟩
-
+        let sum : ℕ := ((faeFibM_old' k) L).2 + ((faeFibM_old' (k+1)) L).2
+        ⟨L ++ [sum], sum⟩  -- subcase when `L` has at least two elements
 
 -- ## From Siddhartha
-
-def faeFibM (n : ℕ) : faeState (List ℕ) ℕ := do
-  ⟨fun L ↦ match L[n]? with
-    | some y => ⟨y, L⟩ -- case when `n < L.length`
+-- Recall: `State' (List ℕ) ℕ := List ℕ → List ℕ × ℕ`
+def faeFibM' (n : ℕ) : State' (List ℕ) ℕ := do
+  fun L ↦ match L[n]? with
+    | some y => ⟨L, y⟩ -- case when `n < L.length`
     | none => match n with -- case when `L.length ≤ n`
-      | 0 => ⟨1, [1]⟩ -- subcase when `L` is empty
-      | 1 => ⟨1, [1, 1]⟩ -- subcase when `L` is a singleton
+      | 0 => ⟨[1], 1⟩ -- subcase when `L` is empty
+      | 1 => ⟨[1, 1], 1⟩ -- subcase when `L` is a singleton
       | k + 2 =>      -- subcase when `L` has at least two elements
-        let (s₁, L') := faeFibM k |>.run L
-        let (s₂, L'') := faeFibM (k + 1) |>.run L'
+        let (L', s₁) := (faeFibM' k) L
+        let (L'', s₂) := (faeFibM' (k + 1)) L'
         let sum : ℕ := s₁ + s₂
-        ⟨sum, L'' ++ [sum]⟩
-    ⟩
+        ⟨L'' ++ [sum], sum⟩
 
-set_option trace.profiler true in
-#eval faeFibM 32
+instance : Repr (State' (List ℕ) ℕ) := ⟨fun f n ↦ instReprNat.reprPrec (f []).2 n⟩
 
-set_option trace.profiler true in
-#eval faeFibM_old 32
+-- set_option trace.profiler true in
+#eval faeFibM' 32
+#eval faeFibM_old' 32
+
+-- set_option trace.profiler true in
+-- #eval faeFibM_old 32
